3c488cc1d939cfbe6ddb1a3e092819972137ca67
MacbookAir <macbookair@MacbookAir-no-MacBook-Air.local>
2020-02-01 06:52:01 +0900

    200201

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { theme } from './materialui/theme'
-import { BrowserRouter as Router } from 'react-router-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import { MuiThemeProvider } from '@material-ui/core/styles';
+// import { theme } from './materialui/theme'
+// import { BrowserRouter as Router } from 'react-router-dom';
 
-import { createBrowserHistory } from 'history';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers/rootReducer';
-import thunk from 'redux-thunk'
-import { createStore, applyMiddleware } from 'redux';
+// import { createBrowserHistory } from 'history';
+// import { Provider } from 'react-redux';
+// import rootReducer from './reducers/rootReducer';
+// import thunk from 'redux-thunk'
+// import { createStore, applyMiddleware } from 'redux';
 
-const middlewares = []
-middlewares.push(thunk)
-const history = createBrowserHistory();
-
-
-// if (process.env.NODE_ENV !== 'production') {
-// } else {
-//   const { logger } = require('redux-logger')
-//   middlewares.push(logger)
-// }
+// const middlewares = []
+// middlewares.push(thunk)
+// const history = createBrowserHistory();
 
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(...middlewares)
-)
+// // if (process.env.NODE_ENV !== 'production') {
+// // } else {
+// //   const { logger } = require('redux-logger')
+// //   middlewares.push(logger)
+// // }
+
+
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(...middlewares)
+// )
 
 
 
-ReactDOM.render(
-  <Provider store={store}>
-    <MuiThemeProvider theme={theme} >
-      <Router history={history}>
-        <App />
-      </Router>
-    </MuiThemeProvider>
-  </Provider >
-  , document.getElementById('root'));
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <MuiThemeProvider theme={theme} >
+//       <Router history={history}>
+//         <App />
+//       </Router>
+//     </MuiThemeProvider>
+//   </Provider >
+//   , document.getElementById('root'));
